0af2ab5f850cc85081ff6b2a38d1ad9e568380e7
Arthur Lara <arthurspider743@gmail.com>
2025-09-10 16:37:26 -0300

    Update register.php

--- register.php
+++ register.php
@@ -6,15 +6,20 @@
     <title>Cadastro</title>
 </head>
 <body>
+
+<h1>Cadastro de novo usuário</h1>
+
     <form method="POST">
         <label>Digite seu nome:</label>
-        <input type="text" name="nome" required>
+        <input type="text" name="nome" required><br><br>
 
         <label>Digite seu email:</label>
-        <input type="novo_email" name="email" required>
+        <input type="email" name="email" required><br><br>
 
         <label>Digite sua senha:</label>
-        <input type="password" name="senha" required>
+        <input type="password" name="senha" required><br><br>
+
+        <hr>
 
         <input type="submit" value="Cadastrar">
     </form>
@@ -23,3 +28,37 @@
 
 </body>
 </html>
+
+<?php
+
+require_once "db_connect.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = trim(string: $_POST["nome"]);
+    $email = trim(string: $_POST["email"]);
+    $senha = trim(string: $_POST["senha"]);
+
+    if ($nome && $email && $senha) {
+        $verifica = $pdo->prepare("SELECT id FROM usuarios WHERE email = ?");
+        $verifica->execute($email);
+
+        //busca linha
+        if ($verifica->fetch()) {
+            echo "Erro ao cadastrar";
+        }
+        else {
+            //criptografar senha
+            $hash = password_hash($senha, PASSWORD_DEFAULT);
+            $stmt = $pdo->prepare("INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)");
+            $stmt->execute([$nome, $email, $senha]);
+
+            echo "Cadastro realizado.";
+        }
+    }
+    else {
+        echo "Preencha os campos.";
+    }
+}
+
+
+?>
