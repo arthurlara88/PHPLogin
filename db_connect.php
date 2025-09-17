9ba6e478e0a1d6658d339731acbb6182b78c4d4d
Arthur Lara <arthurspider743@gmail.com>
2025-09-17 12:01:50 -0300

    meu codigo

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,12 @@
 <?php
 
 $server = "newton.coltec.ufmg.br";
-$username = "a2024951923@teiacoltec.org"; // usuario
-$password = "";
-$dbname = "atividade"; //nome do seu banco de dados
+$username = "a2024953241@teiacoltec.org"; // usuario
+$password = "a2024953241";
+$dbname = "a2024953241@teiacoltec.org"; //nome do seu banco de dados
 
 //Tenta estabelecer conexao com o phpmyadmin
-// Senao ele so encerra o processo
+//Senao ele so encerra o processo
 try {
     $pdo = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8", $username, $password);
 
--- index.php
+++ index.php
@@ -10,11 +10,10 @@
     <div class="container">
         <h1>Seja bem-vindo</h1>
 
-        <p>Efetue o login para acessar o sistema.</p>
-
+        <p>Efetue o login para acessar o sistema:</p>
         <p><a href="login.php">Efetuar login</a></p>
 
-        <p>Cadastre um professor.</p>
+        <p>Cadastre um professor:</p>
 
         <p><a href="register.php">Cadastrar</a></p>
     </div>
